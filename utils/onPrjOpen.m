e3cd6b498c6a79420f3044747e505877766f677c
Isaac Ito <isaacito@mathworks.com>
2021-03-24 13:35:50 +0000

    Update the Project settings.

---
+++ utils/onPrjOpen.m
@@ -0,0 +1,14 @@
+% This script is automatically run when the MATLAB Project is opened.
+% To change the automatic execution setting, select the Project window,
+% and in the PROJECT toolstrip, click Startup Shutdown button.
+%
+% Copyright 2020-2021 The MathWorks, Inc.
+
+v = ver('matlab');
+if not(v.Version == "9.9")
+  release = "R2020b";
+  disp("This project was developed in " + release + ",")
+  disp("This MATLAB is " + v.Release(2:end-1) + ".")
+  clear release
+end
+clear v
