08a9cce6450ef7b02c685ce9b31aab12acd30f54
Isaac Ito <isaacito@mathworks.com>
2021-03-24 14:27:03 +0000

    Add untracked files to git and the Project.

---
+++ HEV/PowerSplitHEV_SpeedTracking/utils/PowerSplitHEV_SpeedTracking_select_simple_pattern.m
@@ -0,0 +1,22 @@
+%% Select Input Pattern for Power-Split HEV with Speed Tracking
+
+% Copyright 2021 The MathWorks, Inc.
+
+mdl = "PowerSplitHEV_SpeedTracking";
+if not(bdIsLoaded(mdl)), open_system(mdl); end
+
+input_pattern = "simple_drive_pattern";
+disp("Selecting " + input_pattern + " (input_pattern workspace variable)")
+
+% Longitudinal vehicle speed reference
+[inputSignals_DrivingPattern, inputBus_DrivingPattern, initial_DrvPtn, opt1] = ...
+  DrivingPatternBasic_inputs("InputPattern", input_pattern);
+
+drivingPattern.useFromWorkspace_tf = opt1.useFromWorkspace;
+
+% Road slope
+[inputSignals_RoadGrade, inputBus_RoadGrade, opt2] = ...
+  DriverAndEnvironment_road_grade_pattern("InputPattern", "flat");
+
+% Simulation time
+t_end = max(opt1.t_end, opt2.t_end);
